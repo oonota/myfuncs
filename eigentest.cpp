3329c22e96081a46cbe17cc8b8e591071e0f10b6
oono ta <taotaotao0310@gmail.com>
2017-12-28 16:30:22 +0900

    eigen test

---
+++ eigentest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#include"Eigen/Core"
+
+int main(){
+
+    cout << "Hello World" << endl;
+
+    Eigen::MatrixXf A = Eigen::MatrixXf::Zero(2,2);
+    A(0,0) = 2;
+    A(1,1) = 5;
+
+    cout << A << endl;
+
+    return 0;
+}
